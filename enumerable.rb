e949683b71beb0c001ee018b4ba7a5022a7a38a9
Ibrahim Dauda <ibrolive@gmail.com>
2019-07-30 17:03:34 +0100

    added each with index function

--- enumerable.rb
+++ enumerable.rb
@@ -10,5 +10,17 @@ module Enumerable
       end
 
     end
+
+    #my_each_with_index
     
+    def my_each_with_index
+
+      for i in 0...self.length
+
+         yield(self[i], i)
+
+      end
+
+    end
+
  end
